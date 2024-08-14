be8b10f68f0b839f3ff2af903acebba55e280f71
Dinath01 <dinithnethmina101@gmail.com>
2024-08-14 13:18:35 +0530

    an api fetch for the blog page

--- src/Components/postCard/postCard.jsx
+++ src/Components/postCard/postCard.jsx
@@ -2,7 +2,7 @@ import styles from './postCard.module.css';
 import Image from "next/image";
 import Link from 'next/link';
 
-const postCard = () => {
+const postCard = ({post}) => {
   return (
     <div className={styles.conatiner}>
         <div className={styles.top}>
@@ -12,8 +12,8 @@ const postCard = () => {
             <span className={styles.date}>01.01.2024</span>
         </div>
         <div className={styles.bottom}>
-            <h1 className={styles.title}>Title</h1>
-            <p className={styles.desc}>Description</p>
+            <h1 className={styles.title}>{post.title}</h1>
+            <p className={styles.desc}>{post.body}</p>
             <Link className={styles.link} href = '/blog/post'>Read More</Link>
         </div>
     </div>
